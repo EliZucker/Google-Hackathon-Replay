099a4d986b2c5e464c77ea433a12b4b80894b6aa
EliZucker <thezuck@google.com>
2019-07-26 22:17:10 -0400

    basic pipeline attmept 4

--- Main.py
+++ Main.py
@@ -3,6 +3,7 @@ import audiotest2
 import sys
 import os
 import display_on_rpi
+import time
 
 
 os.environ["GOOGLE_APPLICATION_CREDENTIALS"]="/home/pi/replayml-abeb9b1d988e.json"
@@ -10,4 +11,4 @@ audiotest2.record(sys.argv[1])
 output_str = cloud_conn.transcribe_file(sys.argv[1]+'.wav')
 display_on_rpi.init_display()
 display_on_rpi.display_text(output_str)
-sleep(10)
+time.sleep(10)
