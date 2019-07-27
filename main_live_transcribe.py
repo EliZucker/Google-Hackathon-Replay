94322ed4b4bb46079fadf87bec970e1d9be531f7
Arjun D'Cunha <arjundcunha@Arjuns-MacBook-Pro-3.local>
2019-07-27 03:13:44 -0400

    third queue

--- main_live_transcribe.py
+++ main_live_transcribe.py
@@ -7,10 +7,10 @@ from os import listdir, remove, environ
 from os.path import isfile, join
 
 def main():
-	remove("/home/pi/Google-Hackathon-Replay/text/log.txt")
+	# remove("/home/pi/Google-Hackathon-Replay/text/log.txt")
 	environ["GOOGLE_APPLICATION_CREDENTIALS"]="/home/pi/replayml-abeb9b1d988e.json"
 	mypath = '/home/pi/Google-Hackathon-Replay/audio/'
-	f=open("/home/pi/Google-Hackathon-Replay/text/log.txt", "a+", "os.O_NONBLOCK")
+	f=open("/home/pi/Google-Hackathon-Replay/text/log.txt", "a", "os.O_NONBLOCK")
 	while True:
 		onlyfiles = [f for f in listdir(mypath) if isfile(join(mypath, f))]
 		for file in onlyfiles:
