8386d2cd35cd5012879ba3f8cfb06bf5a147aef6
Arjun D'Cunha <arjundcunha@Arjuns-MacBook-Pro-3.local>
2019-07-26 23:53:35 -0400

    multiple process

--- Main.py
+++ Main.py
@@ -4,11 +4,16 @@ import sys
 import os
 import display_on_rpi
 import time
+import subprocess
 
+def main():
+	os.environ["GOOGLE_APPLICATION_CREDENTIALS"]="/home/pi/replayml-abeb9b1d988e.json"
+	count = 0
+	subprocess.call("python main_live_transcribe", shell=True)
+	while True:
+		audiotest2.record('/audio/input_audio_'+count)
+		count+=1
+		
 
-os.environ["GOOGLE_APPLICATION_CREDENTIALS"]="/home/pi/replayml-abeb9b1d988e.json"
-audiotest2.record(sys.argv[1])
-output_str = cloud_conn.transcribe_file(sys.argv[1]+'.wav')
-display_on_rpi.init_display()
-display_on_rpi.display_text(output_str)
-time.sleep(10)
+if __name__=="__main__":
+	main()
