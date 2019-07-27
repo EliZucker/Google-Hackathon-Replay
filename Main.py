f8d3983cec4844f136c5ef2360c4ca056d4c95b4
EliZucker <thezuck@google.com>
2019-07-27 00:54:20 -0400

    test buttons

--- Main.py
+++ Main.py
@@ -9,23 +9,13 @@ from pitftgpio import PiTFT_GPIO
 
 def main():
 	os.environ["GOOGLE_APPLICATION_CREDENTIALS"]="/home/pi/replayml-abeb9b1d988e.json"
-    pitft = PiTFT_GPIO 
+        pitft = PiTFT_GPIO(v2=True) 
 	count = 0
 	subprocess.Popen("sudo python /home/pi/Google-Hackathon-Replay/main_live_transcribe.py", stdout=subprocess.PIPE, stdin=subprocess.PIPE, shell=True)
-        print 'after subprocess'
 	while True:
-        if pitft.Button1:
-            print "Button 1 pressed - screen off"
-            pitft.Backlight(False)
-        if pitft.Button2:
-            print "Button 2 pressed - screen on"
-            pitft.Backlight(True) 
-        if pitft.Button3:
-            print "Button 3 pressed"
-        if pitft.Button4:
-            print "Button 4 pressed"
-		audiotest2.record('audio/input_audio_'+str(count))
-		count+=1
+            #if pitft.Button1:
+            audiotest2.record('audio/input_audio_'+str(count))
+            count+=1
 		
 if __name__=="__main__":
 	main()
--- main_live_transcribe.py
+++ main_live_transcribe.py
@@ -11,12 +11,12 @@ from os.path import isfile, join
 def main():
 	os.environ["GOOGLE_APPLICATION_CREDENTIALS"]="/home/pi/replayml-abeb9b1d988e.json"
 	mypath = '/home/pi/Google-Hackathon-Replay/audio/'
+	screen = display_on_rpi.init_display()
 	while True:
 		onlyfiles = [f for f in listdir(mypath) if isfile(join(mypath, f))]
 		for file in onlyfiles:
 			output_str = cloud_conn.transcribe_file('audio/'+file)
-			display_on_rpi.init_display()
-			display_on_rpi.display_text(output_str)
+			display_on_rpi.display_text(output_str, screen)
 			os.remove('audio/'+file)
 
 
