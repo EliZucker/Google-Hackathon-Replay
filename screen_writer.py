f13d8ac4ff46b6d24c23d9e016ccb38517e0673b
Arjun D'Cunha <arjundcunha@Arjuns-MacBook-Pro-3.local>
2019-07-27 10:13:39 -0400

    third queue

--- screen_writer.py
+++ screen_writer.py
@@ -17,7 +17,7 @@ def replay(transcription_file, seconds):
 	with open(transcription_file, 'r', os.O_NONBLOCK) as content_file:
     		content = content_file.read()
 		max_chars_to_look_back = min(seconds*3*8, len(content))
-		replayChunk = content[len(total_transcription)-max_chars_to_look_back:]
+		replayChunk = content[len(content)-max_chars_to_look_back:]
 		wordsInChunk = replayChunk.split()
 		max_words_to_replay = min(seconds*3, len(wordsInChunk))
 		return wordsInChunk[len(wordsInChunk)-max_words_to_replay:]
