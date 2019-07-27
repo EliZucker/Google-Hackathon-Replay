bbae1a06561fa199ec1ac77911630245d738453f
Arjun D'Cunha <arjundcunha@Arjuns-MacBook-Pro-3.local>
2019-07-26 21:27:28 -0400

    Creating main

--- Main.py
+++ Main.py
@@ -1,66 +1,10 @@
-# we need to fill this up!
-from google.cloud import texttospeech
-from google.cloud import speech
-from google.cloud.speech import enums
-from google.cloud.speech import types
-import io
-client = speech.SpeechClient()
+import cloud_conn
+import audiotest2
+import display_on_rpi
 
-def transcribe_file(speech_file):
-    """Transcribe the given audio file."""
 
-    # [START speech_python_migration_sync_request]
-    # [START speech_python_migration_config]
-    with io.open(speech_file, 'rb') as audio_file:
-        content = audio_file.read()
+audiotest2.record(sys.argv[1])
+output_str = cloud_conn.transcribe_file(sys.argv[1]+'.wav')
 
-    audio = types.RecognitionAudio(content=content)
-    config = types.RecognitionConfig(
-        encoding=enums.RecognitionConfig.AudioEncoding.LINEAR16,
-        sample_rate_hertz=44100,
-        language_code='en-US')
-    # [END speech_python_migration_config]
 
-    # [START speech_python_migration_sync_response]
-    response = client.recognize(config, audio)
-    # [END speech_python_migration_sync_request]
-    # Each result is for a consecutive portion of the audio. Iterate through
-    # them to get the transcripts for the entire audio file.
-    for result in response.results:
-        # The first alternative is the most likely one for this portion.
-        return result.alternatives[0].transcript
-    # [END speech_python_migration_sync_response]
-# [END speech_transcribe_sync]
-def textToSpeech(line):
-# Instantiates a client
-    tts_client = texttospeech.TextToSpeechClient()
-
-# Set the text input to be synthesized
-    synthesis_input = texttospeech.types.SynthesisInput(text=line)
-
-# Build the voice request, select the language code ("en-US") and the ssml
-# voice gender ("neutral")
-    voice = texttospeech.types.VoiceSelectionParams(
-        language_code='en-US',
-        ssml_gender=texttospeech.enums.SsmlVoiceGender.NEUTRAL)
-
-# Select the type of audio file you want returned
-    audio_config = texttospeech.types.AudioConfig(
-        audio_encoding=texttospeech.enums.AudioEncoding.MP3)
-
-# Perform the text-to-speech request on the text input with the selected
-# voice parameters and audio file type
-    response = tts_client.synthesize_speech(synthesis_input, voice, audio_config)
-
-    # The response's audio_content is binary.
-    with open('output.mp3', 'wb') as out:
-        # Write the response to the output file.
-        out.write(response.audio_content)
-        print('Audio content written to file "output.mp3"')
-def main():
-    line = transcribe_file("/home/brian_oppenheim/Google-Hackathon-Replay/voice4.wav")
-    print(line)
-    #textToSpeech("home/brian_oppenheim/Google-Hackathon-Replay/voice4.wav")
-
-if __name__ == "__main__":
-    main()
+display_on_rpi(output_str)
--- audiotest2.py
+++ audiotest2.py
@@ -1,38 +1,40 @@
 import pyaudio
 import wave
 
-CHUNK = 4096
-FORMAT = pyaudio.paInt16
-CHANNELS = 1
-RATE = 44100
-RECORD_SECONDS = 10
-WAVE_OUTPUT_FILENAME = "voice.wav"
 
-p = pyaudio.PyAudio()
+def record(filename):
+	CHUNK = 4096
+	FORMAT = pyaudio.paInt16
+	CHANNELS = 1
+	RATE = 44100
+	RECORD_SECONDS = 10
+	# WAVE_OUTPUT_FILENAME = "voice.wav"
 
-stream = p.open(format=FORMAT,
-                channels=CHANNELS,
-                rate=RATE,
-                input=True,
-                frames_per_buffer=CHUNK)
+	p = pyaudio.PyAudio()
 
-print("* recording")
+	stream = p.open(format=FORMAT,
+	                channels=CHANNELS,
+	                rate=RATE,
+	                input=True,
+	                frames_per_buffer=CHUNK)
 
-frames = []
+	print("* recording")
 
-for i in range(0, int(RATE / CHUNK * RECORD_SECONDS)):
-    data = stream.read(CHUNK)
-    frames.append(data)
+	frames = []
 
-print("* done recording")
+	for i in range(0, int(RATE / CHUNK * RECORD_SECONDS)):
+	    data = stream.read(CHUNK)
+	    frames.append(data)
 
-stream.stop_stream()
-stream.close()
-p.terminate()
+	print("* done recording")
 
-wf = wave.open(WAVE_OUTPUT_FILENAME, 'wb')
-wf.setnchannels(CHANNELS)
-wf.setsampwidth(p.get_sample_size(FORMAT))
-wf.setframerate(RATE)
-wf.writeframes(b''.join(frames))
-wf.close()
+	stream.stop_stream()
+	stream.close()
+	p.terminate()
+
+	wf = wave.open(filename+'.wav', 'wb')
+	wf.setnchannels(CHANNELS)
+	wf.setsampwidth(p.get_sample_size(FORMAT))
+	wf.setframerate(RATE)
+	wf.writeframes(b''.join(frames))
+	wf.close()
