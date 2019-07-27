5ebf6657578e5ac8798ecbbc75f4f0af20638a82
Arjun D'Cunha <arjundcunha@Arjuns-MacBook-Pro-3.local>
2019-07-27 11:50:55 -0400

    third queue

--- cloud_conn.py
+++ cloud_conn.py
@@ -53,7 +53,7 @@ def textToSpeech(line):
 # Perform the text-to-speech request on the text input with the selected
 # voice parameters and audio file type
     response = tts_client.synthesize_speech(synthesis_input, voice, audio_config)
-
+    print 'Received a response and saving it now'
     # The response's audio_content is binary.
     with open('/home/pi/Google-Hackathon-Replay/output.mp3', 'wb') as out:
         # Write the response to the output file.
