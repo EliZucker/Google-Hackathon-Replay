4669d3e59865826f7b508c0f34546590ad8d5466
Arjun D'Cunha <arjundcunha@Arjuns-MacBook-Pro-3.local>
2019-07-27 11:00:36 -0400

    third queue

--- screen_writer.py
+++ screen_writer.py
@@ -78,6 +78,7 @@ def main():
 
 if __name__=="__main__":
 	os.system('sudo rm -rf review/')
+	os.system('sudo mkdir review')
 	subprocess.Popen("sudo python /home/pi/Google-Hackathon-Replay/main_live_transcribe.py", stdout=subprocess.PIPE, stdin=subprocess.PIPE, shell=True)
 	subprocess.Popen("sudo python /home/pi/Google-Hackathon-Replay/Main.py", stdout=subprocess.PIPE, stdin=subprocess.PIPE, shell=True)
 	main()
