ed5425bda0b5ba907ebfccc9cd274163c899e5ca
Arjun D'Cunha <arjundcunha@Arjuns-MacBook-Pro-3.local>
2019-07-27 11:02:17 -0400

    third queue

--- main_live_transcribe.py
+++ main_live_transcribe.py
@@ -11,12 +11,14 @@ def main():
 	#remove("/home/pi/Google-Hackathon-Replay/text/log.txt")
 	environ["GOOGLE_APPLICATION_CREDENTIALS"]="/home/pi/replayml-abeb9b1d988e.json"
 	mypath = '/home/pi/Google-Hackathon-Replay/audio/'
-	log_file=open("/home/pi/Google-Hackathon-Replay/text/log.txt", "a+", os.O_NONBLOCK)
+	
 	while True:
 		onlyfiles = [f for f in listdir(mypath) if isfile(join(mypath, f))]
 		for file in onlyfiles:
 			output_str = cloud_conn.transcribe_file('audio/'+file)
+			log_file=open("/home/pi/Google-Hackathon-Replay/text/log.txt", "a+", os.O_NONBLOCK)
 			log_file.write(" " + output_str)
+			log_file.close()
 			remove('audio/'+file)
 
 if __name__=="__main__":
